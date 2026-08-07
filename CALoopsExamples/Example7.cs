6e934ddb665c34d9d3f9cfb58e6a0230962af8ed
Srikanth Rallabandi <codewithsrikanth@hotmail.com>
2026-08-07 21:24:00 +0530

    Arrays Examples

--- CALoopsExamples/Example7.cs
+++ CALoopsExamples/Example7.cs
@@ -1,7 +1,25 @@
-﻿namespace CALoopsExamples
+﻿using System;
+
+namespace CALoopsExamples
 {
     class Example7
     {
+        static void Main()
+        {
+            int[,] arr = new int[3, 4] { {12,23,45,34 },{34,54,56,77 },{ 65,67,89,98} };
+            for(int r = 0; r < 3; r++)
+            {
+                for(int c = 0; c < 4; c++)
+                {
+                    Console.Write(arr[r, c] + " ");
+                }
+                Console.WriteLine();
+            }
 
+            //foreach (var item in arr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+        }
     }
 }
---
+++ CALoopsExamples/Example8.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CALoopsExamples
+{
+    class Example8
+    {
+        static void Main()
+        {
+            int[,] sales =
+            {
+                {120,345,543 },
+                {987,647,282 },
+                {876,456,837 }
+            };
+
+            Console.WriteLine("Eleements of Multi Dimentional Array are: ");
+            for (int i = 0; i < sales.GetLength(0); i++) 
+            {
+                Console.WriteLine("Product "+(i+1)+": ");
+                for (int j = 0; j < sales.GetLength(1); j++) 
+                {
+                    Console.WriteLine(sales[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
