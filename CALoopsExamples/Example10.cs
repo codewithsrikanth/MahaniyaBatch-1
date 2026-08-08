e2c26a16d7eb09fc716710a260a1c11aeb5833b4
Srikanth Rallabandi <codewithsrikanth@hotmail.com>
2026-08-08 21:03:26 +0530

    Assignment and Examples on Arrays

---
+++ CALoopsExamples/Example10.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CALoopsExamples
+{
+    class Example10
+    {
+        static void Main(string[] args)
+        {
+            int[] marks = {85,12,43,98,13,46,12,65,88 };
+            Console.WriteLine("Elements of array are: ");
+            foreach (int i in marks) 
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Array.Sort(marks);
+            foreach (int i in marks)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Array.Reverse(marks);
+            foreach (int i in marks)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            int index = Array.IndexOf(marks, 43);
+            Console.WriteLine("Index value is: "+index);
+            int lastIndex = Array.LastIndexOf(marks, 12);
+            Console.WriteLine("Last Index is: "+lastIndex);
+
+            int[] copyArray = new int[marks.Length];
+            Array.Copy(marks, copyArray, marks.Length);
+            foreach (int i in copyArray)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Array.Clear(copyArray, 2, 3);
+            foreach (int i in copyArray)
+            {
+                Console.Write(i + " ");
+            }
+            bool result = Array.Exists(copyArray, x => x > 200);
+            Console.WriteLine();
+            Console.WriteLine(result);
+
+            
+        }
+    }
+}
---
+++ CALoopsExamples/Example9.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CALoopsExamples
+{
+    class Example9
+    {
+        static void Main(string[] args)
+        {
+            //Jagged Array
+            string[][] employees =
+            {
+                new string[]{"Rahul", "Priya", "Arjun", "Kiran"},
+                new string[]{ "Anjali", "Ravi" },
+                new string[]{ "Suresh", "Meena", "Vijay" }
+            };
+
+            //Single Dimentional
+            string[] depts = { "IT", "HR", "Finance" };
+
+            for (int i = 0; i < employees.Length; i++)
+            {
+                Console.WriteLine(depts[i]+" Department: ");
+                foreach (var employee in employees[i]) 
+                {
+                    Console.WriteLine(" - " + employee);
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
